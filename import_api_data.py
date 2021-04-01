255d851b5cad8b3fdb32ab1e8388ec03444e5264
sriya21 <37816276+sriya21@users.noreply.github.com>
2021-03-31 22:19:13 -0700

    Creating a function for database connectivity

--- import_api_data.py
+++ import_api_data.py
@@ -5,6 +5,22 @@ import pandas as pd
 from pandas.io.json import json_normalize
 from sqlalchemy import create_engine
 
+
+def db_connection(r, table_name):
+    engine = create_engine(
+        'postgresql://srig:iheartmilkyj3lly@data-candidate-homework.c7iises9xj4e.us-east-1.rds.amazon'
+        'aws.com:5432/cleanser_db')
+    r.head(0).to_sql(table_name, engine, if_exists='replace', index=False)
+    conn = engine.raw_connection()
+    cur = conn.cursor()
+    output = io.StringIO()
+    r.to_csv(output, sep='\t', header=False, index=False)
+    output.seek(0)
+    cur.copy_from(output, table_name, null="")  # null values become ''
+    conn.commit()
+    print("Successful")
+
+
 if __name__ == "__main__":
     zip_file_url = "https://s3.amazonaws.com/data-eng-homework/v1/data.zip"
     r = requests.get(zip_file_url)
@@ -16,25 +32,8 @@ if __name__ == "__main__":
         dfs.append(data.orders)
     temp = pd.concat(dfs, ignore_index=True)
     print(temp)
-
     r = json_normalize(temp)
-    r.drop(columns=['line_items'])
+    r =r.drop(columns = ['line_items'])
+    db_connection(r, 'orders')
     line_items = json_normalize(temp, 'line_items', ['id'], record_prefix='line_items')
-    line_items = pd.io.json.json_normalize(temp.line_items)
-    engine = create_engine(
-        'postgresql://srig:iheartmilkyj3lly@data-candidate-homework.c7iises9xj4e.us-east-1.rds.amazon'
-        'aws.com:5432/cleanser_db')
-    r.head(0).to_sql('orders', engine, if_exists='replace', index=False)
-    conn = engine.raw_connection()
-    cur = conn.cursor()
-    output = io.StringIO()
-    r.to_csv(output, sep='\t', header=False, index=False)
-    output.seek(0)
-    contents = output.getvalue()
-    cur.copy_from(output, 'orders', null="")  # null values become ''
-    line_items.to_csv(output, sep='\t', header=False, index=False)
-    output.seek(0)
-    contents = output.getvalue()
-    cur.copy_from(output, 'line_items', null="")  # null values become ''
-    conn.commit()
-    print("Successful")
+    db_connection(line_items, 'line_items')
