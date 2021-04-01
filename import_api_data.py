e9c39ff4d95555147f3d034d6b616fcfbe778af0
sriya21 <37816276+sriya21@users.noreply.github.com>
2021-04-01 10:57:47 -0700

    converting datatypes and adding a scheduler

--- import_api_data.py
+++ import_api_data.py
@@ -1,14 +1,14 @@
 import json
-
+import time
 import requests, zipfile, io
 import pandas as pd
+import schedule as schedule
 from pandas.io.json import json_normalize
 from sqlalchemy import create_engine
 
 
 def db_connection(r, table_name):
-    engine = create_engine(
-        'postgresql://srig:iheartmilkyj3lly@data-candidate-homework.c7iises9xj4e.us-east-1.rds.amazon'
+    engine = create_engine('postgresql://srig:iheartmilkyj3lly@data-candidate-homework.c7iises9xj4e.us-east-1.rds.amazon'
         'aws.com:5432/cleanser_db')
     r.head(0).to_sql(table_name, engine, if_exists='replace', index=False)
     conn = engine.raw_connection()
@@ -16,12 +16,11 @@ def db_connection(r, table_name):
     output = io.StringIO()
     r.to_csv(output, sep='\t', header=False, index=False)
     output.seek(0)
-    cur.copy_from(output, table_name, null="")  # null values become ''
+    cur.copy_from(output, table_name, null="")
     conn.commit()
     print("Successful")
 
-
-if __name__ == "__main__":
+def daily_job():
     zip_file_url = "https://s3.amazonaws.com/data-eng-homework/v1/data.zip"
     r = requests.get(zip_file_url)
     z = zipfile.ZipFile(io.BytesIO(r.content))
@@ -31,9 +30,25 @@ if __name__ == "__main__":
         data = pd.read_json(z.open(file))
         dfs.append(data.orders)
     temp = pd.concat(dfs, ignore_index=True)
-    print(temp)
     r = json_normalize(temp)
-    r =r.drop(columns = ['line_items'])
+    convert_dict = {'total_price': float,
+                    'subtotal_price': float,
+                    'total_tax': float,
+                    'total_discounts': float,
+                    'total_line_items_price': float,
+                    'total_price_usd': float,
+                    }
+    r = r.astype(convert_dict)
+    r = r.drop(columns=['line_items'])
     db_connection(r, 'orders')
     line_items = json_normalize(temp, 'line_items', ['id'], record_prefix='line_items')
     db_connection(line_items, 'line_items')
+
+
+
+
+if __name__ == "__main__":
+    schedule.every(10).minutes.do(daily_job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
